e2b8859d9081c30057e2a29934449c7123c06a25
andrewlymar <happyandrual@gmail.com>
2020-11-01 20:13:07 +0200

    Up

--- app/js/mobileMenu.js
+++ app/js/mobileMenu.js
@@ -1,144 +1,148 @@
 (function ($) {
-	$.fn.mobileMenu = function (options) {
-		var options = $.extend({}, {
-			menuIconClassName: ".menu-icon",
-			mobileResolution: 768,
-			menuType: "fixed",
-			offsetToSticky: 50,
-			closeIconClassName: ".close-menu-icon"
-		}, options);
+    $.fn.mobileMenu = function (options) {
+        var options = $.extend({}, {
+            menuIconClassName: ".menu-icon",
+            mobileResolution: 768,
+            menuType: "fixed",
+            offsetToSticky: 50,
+            closeIconClassName: ".close-menu-icon"
+        }, options);
 
-		var $menu = $(this);
-		var $menuList = $menu.find("ul");
-		var $menuLinks = $menu.find("a");
-		var $menuIcon = $(options.menuIconClassName);
-		var $closeIcon = $(options.closeIconClassName);
-		var currentMenuHeight = $menu.outerHeight();
-		var documentWidth = $(document).width();
-		var menuHeightFixed;
-		var menuIsOpened = false;
-		var offset = 0;
-		var scrollPos = 0;
+        var $menu = $(this);
+        var $menuList = $menu.find("ul");
+        var $menuLinks = $menu.find("a");
+        var $menuIcon = $(options.menuIconClassName);
+        var $closeIcon = $(options.closeIconClassName);
+        var currentMenuHeight = $menu.outerHeight();
+        var documentWidth = $(document).width();
+        var menuHeightFixed;
+        var menuIsOpened = false;
+        var offset = 0;
+        var scrollPos = 0;
+        var isFirstScroll = true;
 
-		if (documentWidth >= options.mobileResolution) {
-			if (options.menuType == "sticky") {
-				$menu.addClass("sticky-menu");
-				currentMenuHeight = $menu.outerHeight() * 2;
-				menuHeightFixed = currentMenuHeight;
-				offset = currentMenuHeight;
-				setTimeout(function () {
-					$menu.removeClass("sticky-menu");
-				}, 1)
-			}
-			if (options.menuType == "fixed") {
-				currentMenuHeight = $menu.outerHeight();
-				offset = currentMenuHeight;
-			}
-			if (options.menuType == "custom") {
-				currentMenuHeight = $menu.outerHeight();
-				offset = currentMenuHeight;
-			}
-		}
+        if (documentWidth >= options.mobileResolution) {
+            if (options.menuType == "sticky") {
+                currentMenuHeight = $menu.outerHeight();
+                menuHeightFixed = currentMenuHeight;
+                offset = currentMenuHeight;
+            }
+            if (options.menuType == "fixed") {
+                currentMenuHeight = $menu.outerHeight();
+                offset = currentMenuHeight;
+            }
+            if (options.menuType == "custom") {
+                currentMenuHeight = $menu.outerHeight();
+                offset = currentMenuHeight;
+            }
+        } else {
+            currentMenuHeight = $menu.outerHeight();
+            offset = currentMenuHeight;
+        }
 
-		$(window).on("resize", onResizeChangeState);
-		$menuIcon.on("click", onClickChangeState);
-		$(document).on("scroll", onScroll);
-		$menuLinks.on("click", scrollTo);
+        $(window).on("resize", onResizeChangeState);
+        $menuIcon.on("click", onClickChangeState);
+        $(document).on("scroll", onScroll);
+        $menuLinks.on("click", scrollTo);
 
-		if ($closeIcon) {
-			$closeIcon.on("click", function () {
-				if (menuIsOpened) {
-					hideMenu();
-				}
-			});
-		}
+        if ($closeIcon) {
+            $closeIcon.on("click", function () {
+                if (menuIsOpened) {
+                    hideMenu();
+                }
+            });
+        }
 
-		function onClickChangeState(event) {
-			if (!menuIsOpened) {
-				showMenu();
-			} else {
-				hideMenu();
-			}
-		}
+        function onClickChangeState(event) {
+            if (!menuIsOpened) {
+                $menu.addClass("sticky-menu");
+                showMenu();
+            } else {
+                hideMenu();
+                if (documentWidth > options.mobileResolution) {
+                    $menu.removeClass("sticky-menu");
+                }
+            }
+        }
 
-		function onResizeChangeState(event) {
-			documentWidth = $(document).width();
-			if (!menuIsOpened && documentWidth > options.mobileResolution) {
-				showMenu();
-			} else if (menuIsOpened && documentWidth <= options.mobileResolution) {
-				hideMenu();
-			}
-		}
+        function onResizeChangeState(event) {
+            documentWidth = $(document).width();
+            if (!menuIsOpened && documentWidth > options.mobileResolution) {
+                showMenu();
+            } else if (menuIsOpened && documentWidth <= options.mobileResolution) {
+                hideMenu();
+            }
+        }
 
-		function onScroll(event) {
-			scrollPos = $(document).scrollTop();
-			if (documentWidth >= options.mobileResolution) {
-				if (options.menuType == "sticky") {
-					fixedMenu();
-				}
-				if (options.menuType == "custom") {
-					customMenu();
-				}
-			}
-			$menuLinks.each(function () {
-				var currLink = $(this);
-				var refElement = $(currLink.attr("href"));
-				if (refElement.position().top <= scrollPos + offset && refElement.position().top + refElement.height() > scrollPos) {
-					$menuLinks.removeClass("active");
-					currLink.addClass("active");
-				} else {
-					currLink.removeClass("active");
-				}
-			});
-		}
+        function onScroll(event) {
+            scrollPos = $(document).scrollTop();
 
-		function scrollTo(event) {
-			var target = this.hash;
-			var $target = $(target);
-			event.preventDefault();
-			if ($(document).width() <= options.mobileResolution) {
-				hideMenu();
-			}
-			$menuLinks.each(function () {
-				$(this).removeClass("active");
-			})
-			$(this).addClass("active");
+            if (options.menuType == "sticky") {
+                fixedMenu();
+            }
+            if (options.menuType == "custom") {
+                customMenu();
+            }
 
-			$("html, body").stop().animate({
-				'scrollTop': $target.offset().top - offset
-			}, 500, "swing", function () {});
-		}
+            if ($(window).scrollTop() <= $(document).height() - $(window).height() - 50) {
+                $menuLinks.each(function () {
+                    var currLink = $(this);
+                    var refElement = $(currLink.attr("href"));
+                    if (refElement.position().top <= scrollPos + offset && refElement.position().top + refElement.height() > scrollPos) {
+                        $menuLinks.removeClass("active");
+                        currLink.addClass("active");
+                    }
+                });
+            } else {
+                $menuLinks.removeClass("active");
+                $menuLinks.last().addClass("active");
+            }
+        }
 
-		function fixedMenu() {
-			if (scrollPos > options.offsetToSticky) {
-				$menu.addClass("sticky-menu");
-				currentMenuHeight = $menu.outerHeight();
-			} else {
-				$menu.removeClass("sticky-menu");
-				currentMenuHeight = menuHeightFixed;
-			}
-			offset = currentMenuHeight;
-		}
+        function scrollTo(event) {
+            var target = this.hash;
+            var $target = $(target);
+            if ($(document).width() <= options.mobileResolution) {
+                hideMenu();
+            }
+            $("html, body").stop().animate({
+                'scrollTop': $target.offset().top - offset + 2
+            }, 500, "swing", function () {});
+        }
 
-		function customMenu() {
-			if (scrollPos > 50) {
-				$menu.addClass("custom-menu");
-			} else {
-				$menu.removeClass("custom-menu");
-			}
-		}
+        function fixedMenu() {
+            if (scrollPos > options.offsetToSticky) {
+                $menu.addClass("sticky-menu");
+                currentMenuHeight = $menu.outerHeight();
+            } else {
+                currentMenuHeight = menuHeightFixed;
+                $menu.removeClass("sticky-menu");
+            }
 
-		function showMenu() {
-			$menuList.css("display", "flex");
-			menuIsOpened = true;
-		}
+            if (documentWidth >= options.mobileResolution) {
+                offset = currentMenuHeight;
+            }
+        }
 
-		function hideMenu() {
-			$menuList.css("display", "none");
-			menuIsOpened = false;
-		}
+        function customMenu() {
+            if (scrollPos > 50) {
+                $menu.addClass("custom-menu");
+            } else {
+                $menu.removeClass("custom-menu");
+            }
+        }
 
-		return this;
-	};
+        function showMenu() {
+            $menuList.css("display", "flex");
+            menuIsOpened = true;
+        }
+
+        function hideMenu() {
+            $menuList.css("display", "none");
+            menuIsOpened = false;
+        }
+
+        return this;
+    };
 
 })(jQuery);
--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,134 @@
-var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	cleanCSS = require('gulp-clean-css'),
-	rename = require('gulp-rename'),
-	del = require('del'),
-	autoprefixer = require('gulp-autoprefixer'),
-	notify = require("gulp-notify");
-
-gulp.task('js', function () {
-	return gulp.src([
-		'app/js/mobileMenu.js'
-		])
-		.pipe(concat('mobileMenu.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('app/js'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
-});
-
-gulp.task('browser-sync', function () {
-	browserSync({
-		server: {
-			baseDir: 'app'
-		},
-		notify: false,
-	});
-});
+const gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify-es').default,
+    cleanCSS = require('gulp-clean-css'),
+    concatCss = require('gulp-concat-css'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache'),
+    svgSprite = require('gulp-svg-sprite'),
+    svgmin = require('gulp-svgmin'),
+    cheerio = require('gulp-cheerio'),
+    replace = require('gulp-replace'),
+    autoprefixer = require('gulp-autoprefixer'),
+    notify = require("gulp-notify");
 
 gulp.task('sass', function () {
-	return gulp.src('app/scss/**/*.scss')
-		.pipe(sass({
-			outputStyle: 'expand'
-		}).on("error", notify.onError()))
-		.pipe(rename({
-			suffix: '.min',
-			prefix: ''
-		}))
-		.pipe(autoprefixer(['last 5 versions']))
-		.pipe(cleanCSS())
-		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'expand'
+        }).on("error", notify.onError()))
+        .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('watch', ['sass', 'js', 'browser-sync'], function () {
-	gulp.watch('app/scss/**/*.scss', ['sass']);
-	gulp.watch(['app/js/common.js', 'app/js/mobileMenu.js'], ['js']);
-	gulp.watch('app/*.html', browserSync.reload);
+gulp.task('css', function () {
+    return gulp.src([
+        'app/css/main.css'
+        ])
+        .pipe(autoprefixer(['last 3 versions']))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(concatCss('style.min.css'))
+        .pipe(gulp.dest('app'))
 });
 
-gulp.task('default', ['watch']);
+gulp.task('js', function () {
+    return gulp.src([
+        'app/js/jquery.min.js',
+        'app/js/mobileMenu.js',
+		'app/js/common.js'
+		])
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app'))
+});
+
+gulp.task('imagemin', function () {
+    return gulp.src(['app/img/**/*', '!app/img/**/*.svg'])
+        .pipe(cache(imagemin()))
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('svg', function () {
+    return gulp.src('app/img/*.svg')
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {
+                xmlMode: true
+            }
+        }))
+        .pipe(replace('&gt;', '>'))
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: "../sprite.svg"
+                }
+            }
+        }))
+        .pipe(gulp.dest('app/img'));
+});
+
+gulp.task('clearcache', function () {
+    return cache.clearAll();
+});
+
+gulp.task('removedist', function (done) {
+    done();
+    return del.sync('dist');
+});
+
+gulp.task('move', function (done) {
+    gulp.src([
+		'app/*.html',
+		]).pipe(gulp.dest('dist'));
+
+    gulp.src([
+		'app/style.min.css',
+		]).pipe(gulp.dest('dist'));
+
+    gulp.src([
+		'app/scripts.min.js',
+		]).pipe(gulp.dest('dist'));
+
+    gulp.src([
+		'app/img/sprite.svg',
+		], { allowEmpty: true }).pipe(gulp.dest('dist/img'));
+
+    gulp.src([
+		'app/fonts/**/*',
+		]).pipe(gulp.dest('dist/fonts'));
+    done();
+});
+
+gulp.task('watch', function (done) {
+    browserSync.init({
+        server: "app"
+    });
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('app/css/**/*.css', gulp.series('css'));
+    gulp.watch('app/js/**/*.js', gulp.series('js'));
+    gulp.watch('app/*.html').on("change", browserSync.reload);
+    gulp.watch('app/scss/**/*.scss').on('change', browserSync.reload);
+    gulp.watch('app/css/**/*.css').on('change', browserSync.reload);
+    gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
+    done();
+});
+
+gulp.task('build', gulp.series('removedist', 'sass', 'css', 'js', 'imagemin', 'move'));
+
+gulp.task('default', gulp.series('sass', 'css', 'js', 'watch'));
